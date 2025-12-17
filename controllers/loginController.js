0952d76f3be5941ebf82a06542dec3a095fd7cd2
Mathin <mathin6381@gmail.com>
2025-12-17 10:08:18 +0530

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,4 @@
-import User from "../models/user.js";
+import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
@@ -83,3 +83,4 @@ export const loginUser = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
